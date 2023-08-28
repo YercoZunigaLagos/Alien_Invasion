92b7781dd2256fa059ce4d85cc4d2379664261a3
BaiCoo <yzunigalagos@gmail.com>
2023-08-27 20:09:04 -0400

    0.5v Movimiento de los aliens listos

--- alien_invation.py
+++ alien_invation.py
@@ -40,7 +40,7 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self.bullets.update()
-
+            self._update_aliens()
             self._update_screen()
             #Eliminar balas que desaparecen
             for bullet in self.bullets.copy():
@@ -89,7 +89,7 @@ class AlienInvasion:
         #Determinamos el numero de filas de aliens que entran en la pantalla
         ship_height = self.ship.rect.height
         available_space_y = self.settings.screen_height - (3 * alien_height) - ship_height
-        number_rows = available_space_y // (2 * alien_height)
+        number_rows = available_space_y // (3 * alien_height)
         print(number_rows)
         #realizamos un ciclo el cual nos permita dibujar todas las naves
         for row_number in range(number_rows):
@@ -109,6 +109,29 @@ class AlienInvasion:
         alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
         
         self.aliens.add(alien)
+
+    def _update_aliens(self):
+        """
+        Check if the fleet is at an edge,
+        then update the positions of all aliens in the fleet.
+        """
+        self._check_fleet_edges()
+
+        """Update the positions of all aliens in the fleet."""
+        self.aliens.update()
+
+    def _check_fleet_edges(self):
+        """Respond appropriately if any aliens have reached an edge."""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+    def _change_fleet_direction(self):
+        """Drop the entire fleet and change the fleet's direction."""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
+            
 
 
     def _update_screen(self):
